3a59382f489c27d2bd9b2f69d081b4e1d2319cc7
Laurent Babin <spywen@hotmail.fr>
2015-05-13 22:33:54 +0200

    Console project added

---
+++ SynchronicWorldConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SynchronicWorldConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventService = new SynchronicWorldService.EventServiceClient();
+            var events = eventService.GetAllEvents();
+
+            foreach (var eventt in events.Result)
+            {
+                Console.Write(eventt.Name);
+            }
+
+            Console.WriteLine("FIN");
+            Console.ReadLine();
+        }
+    }
+}
