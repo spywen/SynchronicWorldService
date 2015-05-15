22f71f41298e7a02f89fb17a2a9e185c0ecfb227
Laurent Babin <spywen@hotmail.fr>
2015-05-15 22:47:37 +0200

    WCF Windows Service Installer

---
+++ SynchronicWorldService.Installer/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using SynchronicWorldService;
+
+namespace SynchronicWorldService.Installer
+{
+    public partial class Service1 : ServiceBase
+    {
+        internal static ServiceHost SwServiceHost = null; 
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (SwServiceHost != null)
+            {
+                SwServiceHost.Close();
+            }
+            SwServiceHost = new ServiceHost(typeof(Service1));
+            SwServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (SwServiceHost != null)
+            {
+                SwServiceHost.Close();
+                SwServiceHost = null;
+            }
+        }
+    }
+}
